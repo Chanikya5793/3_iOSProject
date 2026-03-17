fb9d2bccee85fb16b42088bbb6e498a75e3d612e
chanikya5793 <s584483@nwmissouri.edu>
2026-03-17 12:10:56 -0500

    initial Commit

---
+++ Budgeting Apop/Budgeting Apop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budgeting Apop
+//
+//  Created by Thotakura Chanakya on 3/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
