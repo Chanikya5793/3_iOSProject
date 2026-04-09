c51b2adf5306fbe89d740f4955b986bbe2dc255d
chanikya5793 <s584483@nwmissouri.edu>
2026-04-09 12:34:06 -0500

    Add Firebase dependencies and configure Dashboard view controller

---
+++ Budgeting App/Budgeting App/DashboardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DashboardViewController.swift
+//  Budgeting App
+//
+//  Created by Thotakura Chanakya on 4/9/26.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
